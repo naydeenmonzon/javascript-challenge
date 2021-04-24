f3cb828438c3e03f0643ce8a9ca9d8741b89d75e
naydeenmonzon <naydeen.monzon@icloud.com>
2021-04-23 21:04:55 -0700

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,45 +1,32 @@
 // from data.js
-var tbody = d3.select("table");
+var button = d3.select("#button");
+var form = d3.select("#form");
 
-console.log(data);
+var tbody = d3.select("tbody");
+var row = tbody.append("tr");
 
-data.forEach(function(ufoReport) {
-    console.log(ufoReport);
-});
-data.forEach(function(ufoReport) {
-    console.log(ufoReport);
-    var row = tbody.append("tr");
-});
-data.forEach(function(ufoReport) {
-    console.log(ufoReport);
-    var row = tbody.append("tr");
-    
-    Object.entries(ufoReport).forEach(function([key, value]) {
-        console.log(key, value);
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+
+function runEnter() {
+    var inputElement = d3.select("#datesearch");
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+    console.log(data);
+  
+    var filteredData = data.filter(data => data.datetime === inputValue);
+  
+    console.log(filteredData);
+
+    d3.select("tbody")
+    .selectAll("tr")
+    .data(filteredData)
+    .enter()
+    .append("tr")
+    .html(function(d) {
+        return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.state}</td><td>${d.shape}</td><td>${d.durationMinutes}</td><td>${d.comments}</td>`;
     });
-});
-data.forEach(function(ufoReport) {
-    console.log(ufoReport);
-    var row = tbody.append("tr");
-    
-    Object.entries(ufoReport).forEach(function([key, value]) {
-        console.log(key, value);
-        var cell = row.append("td");
-    });
-});
-data.forEach(function(ufoReport) {
-    console.log(ufoReport);
-    var row = tbody.append("tr");
-    Object.entries(ufoReport).forEach(function([key, value]) {
-        console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
-    });
-});
-data.forEach((ufoReport) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoReport).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
-    });
-});
+};
